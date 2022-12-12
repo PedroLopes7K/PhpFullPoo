f42e024f96a152ac5e060a866d82d3c9f47d59d2
PedroLopes7K <pedrohenri3601@gmail.com>
2022-12-12 07:56:06 -0300

    final changes

--- PessoaForm.php
+++ PessoaForm.php
@@ -6,53 +6,48 @@ class PessoaForm
 {
     private $html;
     private $data;
-    
+
     public function __construct()
     {
         $this->html = file_get_contents('html/form.html');
-        $this->data = [ 'id'        => null,
-                        'nome'      => null,
-                        'endereco'  => null,
-                        'bairro'    => null,
-                        'telefone'  => null,
-                        'email'     => null,
-                        'id_cidade' => null];
-        
+        $this->data = [
+            'id'        => '',
+            'nome'      => '',
+            'endereco'  => '',
+            'bairro'    => '',
+            'telefone'  => '',
+            'email'     => '',
+            'id_cidade' => ''
+        ];
+
         $cidades = '';
-        foreach (Cidade::all() as $cidade)
-        {
+        foreach (Cidade::all() as $cidade) {
             $cidades .= "<option value='{$cidade['id']}'> {$cidade['nome']} </option>";
         }
         $this->html = str_replace('{cidades}', $cidades, $this->html);
     }
-    
+
     public function edit($param)
     {
-        try
-        {
+        try {
             $id = (int) $param['id'];
             $this->data = Pessoa::find($id);
-        }
-        catch (Exception $e)
-        {
+        } catch (Exception $e) {
             print $e->getMessage();
         }
     }
-    
+
     public function save($param)
     {
-        try
-        {
+        try {
             Pessoa::save($param);
             $this->data = $param;
             print "Pessoa salva com sucesso";
-        }
-        catch (Exception $e)
-        {
+        } catch (Exception $e) {
             print $e->getMessage();
         }
     }
-    
+
     public function show()
     {
         $this->html = str_replace('{id}', $this->data['id'], $this->html);
@@ -62,10 +57,12 @@ class PessoaForm
         $this->html = str_replace('{telefone}', $this->data['telefone'], $this->html);
         $this->html = str_replace('{email}', $this->data['email'], $this->html);
         $this->html = str_replace('{id_cidade}', $this->data['id_cidade'], $this->html);
-        
-        $this->html = str_replace("option value='{$this->data['id_cidade']}'",
-                                  "option selected=1 value='{$this->data['id_cidade']}'",
-                                  $this->html);
+
+        $this->html = str_replace(
+            "option value='{$this->data['id_cidade']}'",
+            "option selected=1 value='{$this->data['id_cidade']}'",
+            $this->html
+        );
         print $this->html;
     }
 }
--- classes/Cidade.php
+++ classes/Cidade.php
@@ -3,9 +3,15 @@ class Cidade
 {
     public static function all()
     {
-        $conn = new PDO("pgsql:dbname=livro;user=postgres;host=127.0.0.1");
+
+        $ini = parse_ini_file('config/livro.ini');
+        $host = $ini['host'];
+        $name = $ini['name'];
+        $user = $ini['user'];
+        $pass = $ini['pass'];
+        $conn = new PDO("mysql:host={$host};dbname={$name}", $user, $pass);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
+
         $result = $conn->query("SELECT * FROM cidade ORDER BY id");
         return $result->fetchAll();
     }
